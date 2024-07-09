3bef42db13f647ee511ced3b8aba43c4e2299623
taimoor-th1904453 <taimoor.hussain@fau.de>
2024-07-09 10:55:40 +0200

    Update tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # install requirements
-python -m pip install -r ./src/requirements.txt
+python -m pip install -r ./project/src/requirements.txt
 # Run the pytest script
 python ./project/src/tests.py
